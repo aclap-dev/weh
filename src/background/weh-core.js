9adccad8caf10c86e3246416565b28b296c7658d
mig <michel.gutierrez@gmail.com>
2016-11-18 01:39:52 +0100

    added file headers

--- src/background/weh-core.js
+++ src/background/weh-core.js
@@ -1,3 +1,14 @@
+/*
+ * weh - WebExtensions Help
+ *
+ * @summary workflow and base code for developing WebExtensions browser add-ons
+ * @author Michel Gutierrez
+ * @link https://github.com/mi-g/weh
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 
 (function() {
 
