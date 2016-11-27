539f2b34db05207b62738f97c25c64985ec8a5d8
mig <michel.gutierrez@gmail.com>
2016-11-27 17:09:22 +0100

    added support for inspecting storage and preferences

--- src/background/weh-inspect.js
+++ src/background/weh-inspect.js
@@ -14,10 +14,70 @@ weh.inspect = (function() {
                 });
                 break;
             case "weh#inspect-bgui":
+                inspectorId = sender.id;
                 exports.monitorBgUi = message.status;
+                break;
+            case "weh#get-storage":
+                inspectorId = sender.id;
+                GetStorageData(function(data) {
+                    console.info("storage",data);
+                    exports.send({
+                        type: "weh#storage",
+                        storage: data
+                    });
+                });
+                break;
+            case "weh#get-prefs":
+                inspectorId = sender.id;
+                exports.send({
+                    type: "weh#prefs",
+                    prefs: weh.prefs.getAll(),
+                    specs: weh.prefs.getSpecs()
+                });
+                break;
+            case "weh#save-prefs":
+                inspectorId = sender.id;
+                weh.prefs.assign(message.prefs);
                 break;
         }
     });
+
+    function GetStorageData(callback) {
+        var count = 1;
+        var data = {}
+        function Done() {
+            if(--count==0)
+                callback(data);
+        }
+        ["localStorage","sessionStorage"].forEach(function(which) {
+            var storage = window[which];
+            if(storage) {
+                var webStorage = {};
+                for(var i=0; i<storage.length; i++) {
+                    var key = storage.key(i);
+                    var value = storage.getItem(key);
+                    try {
+                        webStorage[key] = JSON.parse(value);
+                    } catch(e) {
+                        webStorage[key] = value;
+                    }
+                }
+                data[which] = webStorage;
+            }
+        });
+        ["local","sync","managed"].forEach(function(which) {
+            var storage = browser.storage[which];
+            if(storage) {
+                count++;
+                storage.get(null,function(items) {
+                    data[which] = items;
+                    Done();
+                });
+            }
+
+        });
+        Done();
+    }
 
     var exports = {
         monitorBgUi: false,
